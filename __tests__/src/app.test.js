c84fbb0b1f548e343e0ccfcc5b129e4f546d6a9d
rezene <rezenecompeng@gmail.com>
2024-12-09 20:26:22 +0100

    test case added

--- __tests__/src/app.test.js
+++ __tests__/src/app.test.js
@@ -1,3 +1,17 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+
+describe('Math operations', () => {
+  describe('Addition', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(add(1, 2)).toBe(3);
+    });
   });
+
+  describe('Subtraction', () => {
+    test('subtracts 5 - 2 to equal 3', () => {
+      expect(subtract(5, 2)).toBe(3);
+    });
+  });
+});
